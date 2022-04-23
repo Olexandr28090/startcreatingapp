b7bf503803540a8a4ee5afebce63b74fd1cd1b43
Alex <lavrik.sasha6@gmail.com>
2022-04-23 15:13:45 +0300

    third commit

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ let personalMovieDB = {
     private: false
 
 };
-const a = prompt("Один з останніх переглянутих фільмів?"),
-    b = prompt("На скільки оціните фільм?"),
-    c = prompt("Один з останніх переглянутих фільмів?"),
-    d = prompt("На скільки оціните фільм?");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один з останніх переглянутих фільмів?");
+    const b = prompt("На скільки оціните фільм?");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        console.log('error')
+        i--;
+    }
+};
+if (personalMovieDB < 10) {
+    console.log("Переглянуто доволі мало фільмів")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Ви класичний глядач")
+} else if (personalMovieDB.count >= 30) {
+    console.log("Ви кіноман")
+} else {
+    console.log("Помилка")
+}
